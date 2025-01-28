b74e56bc0d2a50a3acc0d6fae37b5ffe7bf10341
JoseKingUVG <riv24376@uvg.edu.gt>
2025-01-28 15:57:04 -0600

    Pequeños cambios

---
+++ src/main/java/uvg/StackVector.java
@@ -0,0 +1,35 @@
+package uvg;
+
+public class StackVector implements Stack{
+
+    @Override
+    public void push(Object item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public Object pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public Object peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public boolean empty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'empty'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+    
+}
