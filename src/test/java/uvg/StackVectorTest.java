25c49eca43fe005ecfb7d4463aaa98f317f7d684
Norman <agu24479@uvg.edu.gt>
2025-01-30 07:30:30 -0600

    PruebaUnitaria P3

--- src/test/java/uvg/StackVectorTest.java
+++ src/test/java/uvg/StackVectorTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class StackVectorTest {
+public class StackVectorTest {
     private StackVector<Integer> stack;
 
     @BeforeEach
